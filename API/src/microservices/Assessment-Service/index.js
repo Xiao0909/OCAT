4a2fabab9fb6f2d8111e975f49d51ffe187570aa
Xiao Xiao <xiaox2@mail.uc.edu>
2021-09-02 14:39:56 -0400

    task1 done
    everything on task 1

--- API/src/microservices/Assessment-Service/index.js
+++ API/src/microservices/Assessment-Service/index.js
@@ -3,6 +3,23 @@ const { Assessments } = require(`../Database`);
 exports.submit = async (assessment) => {
   // use the bookshelf model Assessments from API/src/microservices/Database to save
   // the assessment data in the PostgreSQL database
+  assessment.assessment.created_at = new Date();
+  const this_value = assessment.assessment;
+  if (this_value.score == 0 || this_value.score == 1) {
+    this_value.risk_level = `LOW`;
+  } else if (this_value.score == 2 || this_value.score == 3) {
+    this_value.risk_level = `MID`;
+  }
+  else if (this_value.score == 4 || this_value.score == 5) {
+    this_value.risk_level = `HIGH`;
+  }
+  Assessments.forge({
+    cat_date_of_birth: this_value.birth,
+    cat_name: this_value.catName,
+    created_at: this.created_at,
+    risk_level: this_value.risk_level,
+    score: this_value.score,
+  }).save();
 };
 
 exports.getList = () => {
